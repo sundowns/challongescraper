12c55ccd1d4ce8d3a588363cdc96a6955419d4e2
Sam Holmes <samholmesdev@gmail.com>
2015-02-07 18:35:40 +0800

    Finished the Challonge scraping. Added functionality for it to be scheduled as a background task on the server

--- CloudCode/cloud/main.js
+++ CloudCode/cloud/main.js
@@ -24,13 +24,11 @@ Parse.Cloud.define("getMatchesFromTournaments", function(request, response)
 	{
 		var URL = "https://" + userName + ":" + apiKey + "@api.challonge.com/v1/tournaments/";
 		URL += tournaments[i] + "/matches.json";
-		console.log(URL);
 		promises.push(Parse.Cloud.httpRequest
 		({
 			url: URL
 		}).then(function(httpResponse)
 		{
-			console.log(httpResponse);
 			listOfMatches[listOfMatches.length] = httpResponse;
 		}));
 	}
@@ -43,5 +41,43 @@ Parse.Cloud.define("getMatchesFromTournaments", function(request, response)
 		response.error("Failed during get matches");
 	});
 });
+
+Parse.Cloud.define("convertIDsToNames", function(request, response)
+{
+	var playerSet = request.params.playerSet;
+	var promises = [];
+	var participants = [];
+	for (pid in playerSet)
+	{
+		if (!playerSet.hasOwnProperty(pid))
+		{
+			continue;
+		}
+		var bothParts = pid.split(':');
+		var playerID = bothParts[0];
+		var tornyID = bothParts[1];
+
+		var URL = "https://" + userName + ":" + apiKey + "@api.challonge.com/v1/tournaments/";
+		URL += tornyID + "/participants/" + playerID + ".json";
+		console.log(URL);
+		promises.push(Parse.Cloud.httpRequest
+		({
+			url: URL
+		}).then(function(httpResponse)
+		{
+			participants.push(httpResponse);
+		}));
+	}
+
+	Parse.Promise.when(promises).then(function()
+	{
+		response.success(participants);
+	},
+	function (error)
+	{
+		response.error("Failed during get names");
+	});
+});
+
 
 
